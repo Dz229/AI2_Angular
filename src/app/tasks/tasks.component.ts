42af6228370470548f0856afef3dabdf17ed449b
unknown <zapadka797@gmail.com>
2023-12-19 11:23:53 +0100

    lab-e

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -13,20 +13,28 @@ export class TasksComponent implements OnInit {
   public newTask: Task = {};
   public isProcessing = false;
 
-  constructor(private tasksService: TasksService) {}
+  constructor(
+    private tasksService: TasksService
+  ) {
+  }
+
   ngOnInit() {
     this.tasksService.index().subscribe((tasks) => {
       this.tasks = tasks;
     });
     this.isProcessing = false;
   }
+
   addTask() {
     if (this.newTask.title === undefined) {
       return;
     }
+
     this.newTask.completed = false;
     this.newTask.archived = false;
+
     this.tasks.unshift(this.newTask); // optimistic update; try commenting this line off and compare the difference
+
     this.tasksService.post(this.newTask).subscribe((task) => {
       this.newTask = {};
       this.ngOnInit();
@@ -35,15 +43,43 @@ export class TasksComponent implements OnInit {
 
   handleChange(task: Task) {
     this.tasksService.put(task).subscribe({
-      error: err => {alert(err); this.ngOnInit();}});
+      error: err => {
+        alert(err);
+        this.ngOnInit();
+      }
+    });
   }
+
   archiveCompleted() {
     const observables: Observable<any>[] = [];
     for (const task of this.tasks) {
-      if (!task.completed) {continue;}
+      if (!task.completed) {
+        continue;
+      }
+
       task.archived = true;
       observables.push(this.tasksService.put(task));
     }
-    forkJoin(observables).subscribe(() => {this.ngOnInit();});
+
+    // refresh page when all updates finished
+    forkJoin(observables).subscribe(() => {
+      this.ngOnInit();
+    });
+  }
+
+  canArchiveCompleted() {
+    for (const task of this.tasks) {
+      if (task.completed) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  canAddTask() {
+    if (this.isProcessing) {
+      return false;
+    }
+    return !!this.newTask.title;
   }
 }
